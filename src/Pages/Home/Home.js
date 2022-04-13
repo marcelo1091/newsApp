bac6adf703c4a4147c22fd5bc5e53d27f001d354
marcel.rosa <marcel.rosa@globallogic.com>
2022-04-13 09:19:30 +0200

    test

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,15 +1,17 @@
-import Search from "../../Components/Search/Search"
+import Search from "../../Components/Search/Search";
 
 export default function Home() {
-    return (
-        <div className="flex justify-center flex-col">
-            <div className="flex justify-center p-5">
-                <Search />
-            </div>
-            <div className="flex justify-between flex-row">
-                <div className="flex flex-col"></div>
-                <div className="flex flex-col"></div>
-            </div>
+  return (
+    <div className="flex justify-center flex-col">
+      <div className="flex justify-center p-5">
+        <Search />
+      </div>
+      <div className="flex justify-between flex-row">
+        <div className="flex flex-col">
+          <p>aaaaaa</p>
         </div>
-    )
+        <div className="flex flex-col"></div>
+      </div>
+    </div>
+  );
 }
